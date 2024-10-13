f220668e6d814cf704e2aab5bf5ca186fbc9330e
sanhernandezmon <elsantimontano@gmail.com>
2024-10-13 18:12:26 -0500

    wip removed unnecesary code 🚧

--- src/infraestructure/infraestructure.module.ts
+++ src/infraestructure/infraestructure.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SqsService } from './sqs/sqs.service';
-import { IncidentService } from 'src/incident/incident.service';
 import { IncidentModule } from 'src/incident/incident.module';
 
 @Module({
