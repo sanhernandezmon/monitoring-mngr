c6354a8b1b70bce17aff7466c230f91f47b96d65
sanhernandezmon <elsantimontano@gmail.com>
2024-10-13 18:27:23 -0500

    test passing test ✅

--- src/incident/incident.service.spec.ts
+++ src/incident/incident.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IncidentService } from './incident.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Incident } from './incident.schema';
+const mockIncidentModel = {
+  findByIdAndUpdate: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('IncidentService', () => {
   let service: IncidentService;
+  let incidentModel: Model<Incident>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IncidentService],
+      providers: [
+        IncidentService,
+        {
+          provide: getModelToken(Incident.name),
+          useValue: mockIncidentModel,
+        },
+      ],
     }).compile();
 
     service = module.get<IncidentService>(IncidentService);
+    incidentModel = module.get<Model<Incident>>(getModelToken(Incident.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // Additional tests can be added here
 });
--- src/infraestructure/sqs/sqs.service.spec.ts
+++ src/infraestructure/sqs/sqs.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SqsService } from './sqs.service';
+import { ConfigService } from '@nestjs/config/dist/config.service';
+import { IncidentService } from '../../incident/incident.service';
+
+const incidentSercviceMock = jest.fn().mockReturnValue({UpdateIncident: jest.fn(), CreateIncident: jest.fn()})
 
 describe('SqsService', () => {
   let service: SqsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SqsService],
+      providers: [SqsService, ConfigService, {provide: IncidentService, useValue: incidentSercviceMock}],
     }).compile();
 
     service = module.get<SqsService>(SqsService);
